2958e953d8b288f783690cb091a26c17e21eedd4
Anik Singha <aniksingha@outlook.com>
2023-12-25 17:22:01 -0500

    create docker file

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu:22.04
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y python3 python3-pip git
+WORKDIR /app
+COPY . .
+RUN pip3 install -r requirements.txt
+EXPOSE 80
+CMD ["gunicorn", "-b", "0.0.0.0:80", "app:app"]
--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from scripts.utils import getUploadFolder, getOutputFolder, cleanFolders
 import whisper
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = getUploadFolder()
-
 model = whisper.load_model("base")
 
 @app.route('/')
@@ -21,17 +19,18 @@ def upload():
     file = request.files['file']
     filename = secure_filename(file.filename)
     file.save(getUploadFolder() + filename)
-
+ 
     outName = filename.split('.')[0] + '.srt'
 
     createSRTFile(getUploadFolder()+filename, model, outName)
     
     return send_file(getOutputFolder()+ outName, as_attachment=False), 200
 
-@app.route('/delete', methods=['DELETE', 'GET'])
+@app.route('/clean', methods=['GET'])
 def delete():
     cleanFolders()
-    return "200", 200
+    return "Successfully deleted all files", 200
 
 
 #curl -X POST -H "Content-Type: multipart/form-data" -F "file=@C:\Users\aniks\Downloads\x.mp4" http://127.0.0.1:5000
+
